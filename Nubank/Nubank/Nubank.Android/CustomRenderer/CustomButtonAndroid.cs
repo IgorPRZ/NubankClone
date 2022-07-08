3ae73cdc45cf140bb2470e04e4bccda80dd7018b
Igor <42179042+IgorPRZ@users.noreply.github.com>
2022-07-07 21:36:29 -0300

    Adicionando Primeiro Código

---
+++ Nubank/Nubank/Nubank.Android/CustomRenderer/CustomButtonAndroid.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Nubank.Droid.CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomButtonAndroid))]
+namespace Nubank.Droid.CustomRenderer
+{
+
+    public class CustomButtonAndroid: ButtonRenderer
+    {
+        public CustomButtonAndroid(Context context) : base(context)
+        {
+
+        }
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetAllCaps(false);
+            }
+        }
+    }
+}
---
+++ Nubank/Nubank/Nubank/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Nubank
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void switchBool_Toggled(object sender, ToggledEventArgs e)
+        {
+            try
+            {
+                if (switchBool.IsToggled)
+                {
+                    switchBool.ThumbColor = Color.FromHex("#8A05BE"); 
+                }
+                else 
+                {
+                    switchBool.ThumbColor = Color.White;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
